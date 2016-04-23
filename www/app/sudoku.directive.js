dfe9441ad9b66a2b3f03c84ba4fa0e7467ec4918
Shaun Fowler <shaun.fowler@gmail.com>
2016-04-23 16:02:33 -0400

    split template into directive; cleanup css for the border rendering

--- www/app/sudoku.directive.js
+++ www/app/sudoku.directive.js
@@ -1,22 +1,30 @@
 angular.module('SudokuSolver')
-    .directive('myDirective', ['$window',
+    .directive('sudoku', ['$window',
         function ($window) {
             return {
+                templateUrl: 'app/sudoku.html',
+                controllerAs: 'vm',
+                bindToController: {
+                    puzzle: '='
+                },
+                controller: [function () {
+
+                }],
                 link: function (s, e, a) {
 
                     // Resize the element to fit within the window with a 1:1 aspect ratio.
                     // TODO - Vertically center with offeset for the solve button
-                    resize = function () {
-                        var size = ($window.innerHeight > $window.innerWidth)
-                            ? $window.innerWidth - 50
-                            : $window.innerHeight - 230;
-                        a.$set('style', 'width: ' + size + 'px; height: ' + size + 'px;');
-                    }
+                    // resize = function () {
+                    //     var size = ($window.innerHeight > $window.innerWidth)
+                    //         ? $window.innerWidth - 50
+                    //         : $window.innerHeight - 230;
+                    //     a.$set('style', 'width: ' + size + 'px; height: ' + size + 'px;');
+                    // }
 
-                    resize();
-                    angular.element($window).bind('resize', function () {
-                        resize();
-                    });
+                    // resize();
+                    // angular.element($window).bind('resize', function () {
+                    //     resize();
+                    // });
                 }
             };
         }]);
